177fa002215bfe36fd3eae352037f3f87863fbf9
Daniel Miranda <danielkza2@gmail.com>
2011-08-28 23:53:07 -0300

    reworked char_classes

--- EP1/src/char_classes.c
+++ EP1/src/char_classes.c
@@ -1,29 +1,81 @@
-#include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
-#include <wctype.h>
+#include <assert.h>
 #include <limits.h>
 
 #include "utils.h"
-#include "char_vector.h"
-
 #include "char_classes.h"
 
-// These stubs are needed because we cannot take the adddress of library functions directly.
+#if !defined(__STDC_VERSION__) || __STDC_VERSION__ < 199901L
 
-int tr_char_class_expand(wctype_t type, char_vector_t* out) {
-	wint_t ch;
-	int i;
-	char_vector_t* out;
+int isblank(int c) {
+	return c == ' ' || c == '\t';
+}
 
-	if(!type)
-		return 0;
-	
-	for(ch = CHAR_MIN; ch <= CHAR_MAX; ch++) {
-		if(iswctype(ch, type)) {
-			char_vector_append_char(out, (char)ch);
-			i++;
+#endif
+
+char_class_t char_class_get(const char* class_name) {
+	static const struct {
+		const char   *name;
+		char_class_t char_class;
+	} char_classes[] = {
+		{"alnum", CC_ALNUM},
+		{"alpha", CC_ALPHA},
+		{"blank", CC_BLANK},
+		{"cntrl", CC_CNTRL},
+		{"digit", CC_DIGIT},
+		{"graph", CC_GRAPH},
+		{"lower", CC_LOWER},
+		{"print", CC_PRINT},
+		{"punct", CC_PUNCT},
+		{"space", CC_SPACE},
+		{"upper", CC_UPPER},
+		{"xdigit", CC_XDIGIT},
+	};
+
+	size_t i;
+
+	if(!class_name)
+		return CC_INVALID;
+
+	for(i = 0; i < ARRAY_SIZE(char_classes); i++) {
+		if(strcmp(char_classes[i].name, class_name) == 0) {
+			return char_classes[i].char_class;
 		}
 	}
 
-	return i;
+	return CC_INVALID;	
+}
+
+int char_class_check(int c, char_class_t char_class) {
+	assert((unsigned)c <= UCHAR_MAX);
+	
+	switch(char_class) {
+	case CC_ALNUM:
+		return isalnum(c);  
+	case CC_ALPHA:
+		return isalpha(c);
+	case CC_BLANK:
+		return isblank(c);
+	case CC_CNTRL:
+		return iscntrl(c);
+	case CC_DIGIT:
+		return isdigit(c);
+	case CC_GRAPH:
+		return isgraph(c);
+	case CC_LOWER:
+		return islower(c);
+	case CC_PRINT:
+		return isprint(c);
+	case CC_PUNCT:
+		return ispunct(c);
+	case CC_SPACE:
+		return isspace(c);
+	case CC_UPPER:
+		return isupper(c);
+	case CC_XDIGIT:
+		return isxdigit(c);
+	}
+
+	return 0;
 }
--- EP1/src/char_classes.h
+++ EP1/src/char_classes.h
@@ -1,12 +1,20 @@
 #ifndef TR_CHAR_CLASSES_H
 #define TR_CHAR_CLASSES_H
 
-#include <ctype.h>
+#if !defined(__STDC_VERSION__) || __STDC_VERSION__ < 199901L
 
-#include "isblank.h"
+int isblank(int c);
 
-#include "char_vector.h"
+#endif
 
-int tr_char_class_expand(wctype_t type, char_vector_t* out);
-	
+typedef enum {
+	CC_INVALID = 0,
+	CC_ALNUM, CC_ALPHA, CC_BLANK, CC_CNTRL,
+	CC_DIGIT, CC_GRAPH, CC_LOWER, CC_PRINT,
+	CC_PUNCT, CC_SPACE, CC_UPPER, CC_XDIGIT,
+} char_class_t;
+
+char_class_t char_class_get(const char* class_name);
+int char_class_check(int c, char_class_t char_class);
+
 #endif // #ifndef TR_CHAR_CLASSES_H
