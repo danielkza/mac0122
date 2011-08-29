2593207bbd13d29758c710a75465f15066e37bb1
Daniel Miranda <danielkza2@gmail.com>
2011-08-28 23:58:35 -0300

    added pseudo-destructor for char_vector

--- EP1/src/char_vector.c
+++ EP1/src/char_vector.c
@@ -1,12 +1,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "xmalloc.h"
 #include "utils.h"
 #include "char_vector.h"
 
 char_vector_t* char_vector_new(size_t initial_size) {
-	char_vector_t *vec = (char_vector_t*)malloc(sizeof(char_vector_t));
-	if(!vec)
+	char_vector_t *vec = (char_vector_t*)xmalloc(sizeof(*vec));
+	if(vec == NULL)
 		return NULL;
 
 	vec->len = 0;
@@ -23,20 +24,19 @@ char_vector_t* char_vector_new(size_t initial_size) {
 	return vec;
 }
 
-int char_vector_append_char(char_vector_t* dest, const char ch) {
+size_t char_vector_append_char(char_vector_t* dest, const char ch) {
 	return char_vector_append(dest, &ch, 1);
 }
 
-int char_vector_append(char_vector_t* dest, const char* src, size_t len) {
-	size_t i;
-
-	if(!dest || !src || !len)
-		return 0;
-	else if(!char_vector_expand(dest, dest->len + len))
+size_t char_vector_append(char_vector_t* dest, const char* src, size_t len) {
+	if(dest == NULL || src == NULL || !len)
 		return 0;
 
-	for(i = 0; i < len; i++)
-		dest->vector[dest->len++] = src[i];
+	if(!char_vector_expand(dest, dest->len + len))
+		return 0;
+
+	memcpy(dest->vector + dest->len, src, len);
+	dest->len += len;
 
 	return len;
 }
@@ -45,7 +45,7 @@ int char_vector_expand(char_vector_t* vec, size_t desired_size) {
 	char *new_vec;
 	size_t new_size;
 
-	if(!vec || !desired_size)
+	if(vec == NULL || desired_size == 0)
 		return 0;
 
 	if(vec->size >= desired_size)
@@ -60,17 +60,27 @@ int char_vector_expand(char_vector_t* vec, size_t desired_size) {
 	}
 
 	new_vec = (char*)realloc(vec->vector, new_size);
-	if(!new_vec) {
+	if(new_vec == NULL) {
 		// Try to allocate exactly the desired size, maybe we don't have enough
 		// memory for the next power of 2.
 		new_size = desired_size;
 
 		new_vec = (char*)realloc(vec->vector, new_size);
-		if(!new_vec)
+		if(new_vec == NULL)
 			return 0;
 	}
 
 	vec->vector = new_vec;
 	vec->size = new_size;
 	return 1;
+}
+
+void char_vector_free(char_vector_t* vec) {
+	if(vec != NULL) {
+		if(vec->vector != NULL) {
+			free(vec->vector);
+		}
+
+		free(vec);
+	}
 }
--- EP1/src/char_vector.h
+++ EP1/src/char_vector.h
@@ -8,8 +8,8 @@ typedef struct {
 } char_vector_t;
 
 char_vector_t* char_vector_new(size_t initial_size);
-int char_vector_append(char_vector_t* dest, const char* src, size_t len);
-int char_vector_append_char(char_vector_t* dest, const char ch);
+size_t char_vector_append(char_vector_t* dest, const char* src, size_t len);
+size_t char_vector_append_char(char_vector_t* dest, const char ch);
 int char_vector_expand(char_vector_t* vec, size_t desired_size);
-
+void char_vector_free(char_vector_t* vec);
 #endif //#ifndef TR_CHAR_VECTOR_H
